ed65d3cf46511ec7650ad4a72d37cc1bf1a8e94d
BrunoMundim <brunomundimfranco@gmail.com>
2023-07-18 09:59:43 -0300

    Setup Spring Initializr

---
+++ src/main/java/br/com/mundim/RestaurantReservationManagment/RestaurantReservationManagmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.mundim.RestaurantReservationManagment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantReservationManagmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantReservationManagmentApplication.class, args);
+	}
+
+}
